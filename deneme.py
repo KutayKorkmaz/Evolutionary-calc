a2912a9285d1c6901622f2312c48e379e218bb8a
M. Serdar Karaman <Mavrikant@users.noreply.github.com>
2017-04-04 15:01:00 +0300

    first working example

--- deneme.py
+++ deneme.py
@@ -5,19 +5,68 @@ TEMP=270
 L=174
 R=273
 Datas=[]
-def temperature (offl,offr,lgain,gainr):
-	TEMPe=(L+offl)*lgain+(R+offr)*gainr
-	return TEMPe
+
+def temperature (OL,OR,GL,GR):
+	tempr = (L+OL)*GL + (R+OR)*GR
+	return tempr
+
+def create_mutant (List1,List2):
+	OL=float(List1[0]+List2[0])/2
+	OR=float(List1[1]+List2[1])/2
+	GL=float(List1[2]+List2[2])/2
+	Gr=float(List1[3]+List2[3])/2
+	tempr=temperature(OL,OR,GL,GR)
+	remain=abs(tempr-TEMP)	
+	mutant= (OL,OR,GL,GR,remain)
+	return mutant
+
  
 for j in range(10):
 	OL=random.randint(-25,25)
 	OR=random.randint(-25,25)
-	LG=random.uniform(0.1,1.9)
-	GR=random.uniform(0.1,1.9)
-	tempr=temperature(OL,OR,LG,GR)
+	GL=float(random.randint(1,19))/10
+	GR=float(random.randint(1,19))/10
+	tempr=temperature(OL,OR,GL,GR)
 	remain=abs(tempr-TEMP)
-	Datas.append((OL,OR,LG,GR,remain))
-print Datas
-Datas.sort(key=operator.itemgetter(4))
-print '************************************************'
-print Datas
+	Datas.append((OL,OR,GL,GR,remain))
+
+print "Initial data"
+for j in range(len(Datas)):
+	print j, Datas[j]
+
+
+while 1:
+	Datas.sort(key=operator.itemgetter(4))
+	if Datas[0][4]<1:
+		print "\n\n******RESULT*******"
+		print Datas[0]
+		exit()
+	print '************************************************'
+	print "Sorted initial data"
+	for j in range(len(Datas)):
+		print j, Datas[j]
+	Datas=Datas[:5]#Delete last 5
+	print '************************************************'
+	print "Survived data"
+	for j in range(len(Datas)):
+		print j, Datas[j]
+
+	Datas.append(create_mutant(Datas[0],Datas[1]))
+	Datas.append(create_mutant(Datas[0],Datas[2]))
+	Datas.append(create_mutant(Datas[1],Datas[2]))
+	for j in range(3):
+		OL=random.randint(-25,25)
+		OR=random.randint(-25,25)
+		GL=float(random.randint(1,19))/10
+		GR=float(random.randint(1,19))/10
+		tempr=temperature(OL,OR,GL,GR)
+		remain=abs(tempr-TEMP)
+		Datas.append((OL,OR,GL,GR,remain))
+
+	print '************************************************'
+	print "Survived+Mutation data+Rew_rand"
+	for j in range(len(Datas)):
+		print j, Datas[j]
+
+
+
